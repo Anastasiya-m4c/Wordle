793178a2ecca565bfc8356312071a9751d8aa3c3
annie-mac <mcavoy.ana@gmail.com>
2025-04-08 20:39:36 +0100

    review code and correct typos

--- assets/style.js
+++ assets/style.js
@@ -18,12 +18,12 @@ window.onload = function() {
 function initialise() {
     let grid = document.getElementById('grid');
     for (let r=0; r < height; r++) {
-        for (let c = 0; c < width; c ++) {
+        for (let c = 0; c < width; c++) {
             let tile = document.createElement('input'); 
             tile.type = 'text';
-            tile.maxLength = '1'
+            tile.maxLength = '1';
             tile.classList.add('tile');
-            tile.id = `tile${r}${c}`
+            tile.id = `tile${r}${c}`;
             grid.appendChild(tile);
         }
     }
